96809a686cb54a7867a8816e2fd9a200a85dd7ba
Shaxboz <shaxbozali9@gmail.com>
2023-03-10 12:32:11 +0300

    first commit

---
+++ tours/src/data.js
@@ -0,0 +1,56 @@
+  export const data = [
+  {
+    id: 1,
+    name: 'London',
+    info:
+      "Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 Days!",
+    image:
+      'https://kartinkin.net/uploads/posts/2022-03/thumbs/1647048790_65-kartinkin-net-p-kartinki-londona-v-khoroshem-kachestve-73.jpg',
+    price: '2,695',
+  },
+  {
+    id: 2,
+    name: 'London',
+    info:
+      "Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 Days!",
+    image:
+      'https://kartinkin.net/uploads/posts/2022-03/thumbs/1647048790_65-kartinkin-net-p-kartinki-londona-v-khoroshem-kachestve-73.jpg',
+    price: '2,695',
+  },
+  {
+    id: 3,
+    name: 'London',
+    info:
+      "Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 Days!",
+    image:
+      'https://kartinkin.net/uploads/posts/2022-03/thumbs/1647048790_65-kartinkin-net-p-kartinki-londona-v-khoroshem-kachestve-73.jpg',
+    price: '2,695',
+  },
+  {
+    id: 4,
+    name: 'London',
+    info:
+      "Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 Days!",
+    image:
+      'https://kartinkin.net/uploads/posts/2022-03/thumbs/1647048790_65-kartinkin-net-p-kartinki-londona-v-khoroshem-kachestve-73.jpg',
+    price: '2,695',
+  },
+  {
+    id: 5,
+    name: 'London',
+    info:
+      "Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 Days!",
+    image:
+      'https://kartinkin.net/uploads/posts/2022-03/thumbs/1647048790_65-kartinkin-net-p-kartinki-londona-v-khoroshem-kachestve-73.jpg',
+    price: '2,695',
+  },
+  {
+    id: 6,
+    name: 'London',
+    info:
+      "Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 Days!",
+    image:
+      'https://kartinkin.net/uploads/posts/2022-03/thumbs/1647048790_65-kartinkin-net-p-kartinki-londona-v-khoroshem-kachestve-73.jpg',
+    price: '2,695',
+  },
+]
